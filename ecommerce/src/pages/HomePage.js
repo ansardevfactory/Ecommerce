2311a5c803edae5453fb2565b4b508d433aee3fe
ansardevfactory <104754771+ansardevfactory@users.noreply.github.com>
2022-09-14 11:39:21 +0530

    components integrated

---
+++ ecommerce/src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from "react"; 
+import Topcategories from "../components/Topcategories";
+
+export default function HomePage() {
+  return (
+    <> 
+      <Topcategories/>
+    </>
+  );
+}
